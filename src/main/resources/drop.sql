4bfb0ab3307b007bff7b64daee9743341386780a
Dmitry Tebryaev <tebr5923@mail.ru>
2022-01-04 21:40:51 +0700

    refactored scripts

--- src/main/resources/drop.sql
+++ src/main/resources/drop.sql
@@ -1,11 +1,13 @@
+DROP TABLE IF EXISTS groups_courses;
+DROP TABLE IF EXISTS lectures;
 DROP TABLE IF EXISTS teachers;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS classrooms;
-DROP TABLE IF EXISTS groups_courses;
 DROP SEQUENCE IF EXISTS teachers_id_seq;
 DROP SEQUENCE IF EXISTS students_id_seq;
 DROP SEQUENCE IF EXISTS groups_id_seq;
 DROP SEQUENCE IF EXISTS courses_id_seq;
 DROP SEQUENCE IF EXISTS classrooms_id_seq;
+DROP SEQUENCE IF EXISTS lectures_id_seq;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
+DROP TABLE IF EXISTS groups_courses;
 DROP TABLE IF EXISTS lectures;
 DROP TABLE IF EXISTS teachers;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS classrooms;
-DROP TABLE IF EXISTS groups_courses;
 DROP SEQUENCE IF EXISTS teachers_id_seq;
 DROP SEQUENCE IF EXISTS students_id_seq;
 DROP SEQUENCE IF EXISTS groups_id_seq;
@@ -16,81 +16,59 @@ CREATE SEQUENCE teachers_id_seq START 1;
 
 CREATE TABLE teachers
 (
-    id         int         NOT NULL PRIMARY KEY,
-    first_name varchar(50) NOT NULL,
-    last_name  varchar(50) NOT NULL
+    id         SERIAL PRIMARY KEY,
+    first_name varchar(50)                            NOT NULL,
+    last_name  varchar(50)                            NOT NULL
 );
-
-
-ALTER TABLE teachers
-    ALTER COLUMN id
-        SET DEFAULT NEXTVAL('teachers_id_seq');
 
 
 CREATE SEQUENCE groups_id_seq START 1;
 
 CREATE TABLE groups
 (
-    id   int        NOT NULL PRIMARY KEY,
-    name varchar(5) NOT NULL
+    id   int DEFAULT NEXTVAL('groups_id_seq') NOT NULL PRIMARY KEY,
+    name varchar(5)                           NOT NULL
 );
-
-ALTER TABLE groups
-    ALTER COLUMN id
-        SET DEFAULT NEXTVAL('groups_id_seq');
 
 
 CREATE SEQUENCE students_id_seq START 1;
 
 CREATE TABLE students
 (
-    id         int         NOT NULL PRIMARY KEY,
+    id         int DEFAULT NEXTVAL('students_id_seq') NOT NULL PRIMARY KEY,
     group_id   int,
-    first_name varchar(50) NOT NULL,
-    last_name  varchar(50) NOT NULL,
+    first_name varchar(50)                            NOT NULL,
+    last_name  varchar(50)                            NOT NULL,
     FOREIGN KEY (group_id)
         REFERENCES groups (id)
         ON DELETE CASCADE
 );
-
-ALTER TABLE students
-    ALTER COLUMN id
-        SET DEFAULT NEXTVAL('students_id_seq');
-
 
 
 CREATE SEQUENCE courses_id_seq START 1;
 
 CREATE TABLE courses
 (
-    id   int         NOT NULL PRIMARY KEY,
-    name varchar(50) NOT NULL
+    id   int DEFAULT NEXTVAL('courses_id_seq') NOT NULL PRIMARY KEY,
+    name varchar(50)                           NOT NULL
 );
-
-ALTER TABLE courses
-    ALTER COLUMN id
-        SET DEFAULT NEXTVAL('courses_id_seq');
 
 
 CREATE SEQUENCE classrooms_id_seq START 1;
 
 CREATE TABLE classrooms
 (
-    id     int NOT NULL PRIMARY KEY,
-    number int NOT NULL
+    id     int DEFAULT NEXTVAL('classrooms_id_seq') NOT NULL PRIMARY KEY,
+    number int                                      NOT NULL
 );
-
-ALTER TABLE classrooms
-    ALTER COLUMN id
-        SET DEFAULT NEXTVAL('classrooms_id_seq');
 
 
 CREATE SEQUENCE lectures_id_seq START 1;
 
 CREATE TABLE lectures
 (
-    id           int NOT NULL PRIMARY KEY,
-    date_time timestamptz,
+    id           int DEFAULT NEXTVAL('lectures_id_seq') NOT NULL PRIMARY KEY,
+    date_time    timestamptz,
     classroom_id int,
     teacher_id   int,
     group_id     int,
@@ -109,15 +87,11 @@ CREATE TABLE lectures
         ON DELETE CASCADE
 );
 
-ALTER TABLE lectures
-    ALTER COLUMN id
-        SET DEFAULT NEXTVAL('lectures_id_seq');
-
 
 CREATE TABLE groups_courses
 (
-    group_id int NOT NULL,
-    course_id  int NOT NULL,
+    group_id  int NOT NULL,
+    course_id int NOT NULL,
     PRIMARY KEY (group_id, course_id),
     FOREIGN KEY (group_id)
         REFERENCES groups (id)
