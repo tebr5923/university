688f466556e8eda5efd96a166061529c7e28c1f2
Dmitry Tebryaev <tebr5923@mail.ru>
2022-01-06 00:26:36 +0700

    add save case

--- src/test/java/com/foxminded/university/dao/JdbcTemplateTeacherDaoImplTest.java
+++ src/test/java/com/foxminded/university/dao/JdbcTemplateTeacherDaoImplTest.java
@@ -1,21 +1,26 @@
 package com.foxminded.university.dao;
 
 import com.foxminded.university.config.UniversityConfigTest;
+import com.foxminded.university.domain.model.Teacher;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringJUnitConfig(UniversityConfigTest.class)
 class JdbcTemplateTeacherDaoImplTest {
     public static final String TEACHER_SCRIPT = "teacher.sql";
 
     @Autowired
-    private ApplicationContext applicationContext;
-    @Autowired
     private DBFactory dbFactory;
+    @Autowired
+    private TeacherDao teacherDao;
 
     @BeforeEach
     void setUp() {
@@ -29,9 +34,28 @@ class JdbcTemplateTeacherDaoImplTest {
     }
 
     @Test
-    void getById() {
-        JdbcTemplateTeacherDaoImpl teacherDao =
-                applicationContext.getBean(JdbcTemplateTeacherDaoImpl.class);
-        System.out.println(teacherDao.getAll());
+    void getById_shouldReturnTeacher_whenGetTeacherWhichExist() {
+        Teacher expected = new Teacher();
+        expected.setId(1);
+        expected.setFirstName("Petr");
+        expected.setLastName("Ivanov");
+
+        Optional<Teacher> actual = teacherDao.getById(1);
+
+        assertTrue(actual.isPresent());
+        assertEquals(expected, actual.get());
+    }
+
+    @Test
+    void save_shouldSaveTeacher_whenSavingTeacherNotExist(){
+        Teacher expected = new Teacher();
+        expected.setFirstName("NewFirstName");
+        expected.setLastName("NewFirstName");
+
+        teacherDao.save(expected);
+        Optional<Teacher> actual = teacherDao.getById(expected.getId());
+
+        assertTrue(actual.isPresent());
+        assertEquals(expected, actual.get());
     }
 }
