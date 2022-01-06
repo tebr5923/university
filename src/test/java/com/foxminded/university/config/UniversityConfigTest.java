3dc3c1557706b2d332137789437730b10e6b70e3
Dmitry Tebryaev <tebr5923@mail.ru>
2022-01-06 13:01:48 +0700

    clean UniversityConfigTest

--- src/test/java/com/foxminded/university/config/UniversityConfigTest.java
+++ src/test/java/com/foxminded/university/config/UniversityConfigTest.java
@@ -1,60 +1,10 @@
 package com.foxminded.university.config;
 
-import com.foxminded.university.domain.model.Teacher;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.foxminded.university")
 public class UniversityConfigTest {
-    @Value("${db.driver}")
-    private String driver;
-
-    @Value("${db.url}")
-    private String url;
-
-    @Value("${db.user}")
-    private String user;
-
-    @Value("${db.password}")
-    private String password;
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setDriverClassName(driver);
-        dataSource.setUrl(url);
-        dataSource.setUsername(user);
-        dataSource.setPassword(password);
-
-        return dataSource;
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
-    }
-
-    @Bean
-    public SimpleJdbcInsert insertTeacher() {
-        return new SimpleJdbcInsert(dataSource())
-                .withTableName("teachers")
-                .usingColumns("first_name", "last_name")
-                .usingGeneratedKeyColumns("id");
-    }
-
-    @Bean
-    public BeanPropertyRowMapper<Teacher> teacherRowMapper() {
-        return new BeanPropertyRowMapper<>(Teacher.class);
-    }
 
 }
