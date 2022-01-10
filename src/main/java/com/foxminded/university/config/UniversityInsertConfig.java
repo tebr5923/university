5a82617c55bdc865e2ef1d717f70d2fd107e2d70
Dmitry Tebryaev <tebr5923@mail.ru>
2022-01-10 20:49:52 +0700

    inject DataSource as a method argument

--- src/main/java/com/foxminded/university/config/UniversityInsertConfig.java
+++ src/main/java/com/foxminded/university/config/UniversityInsertConfig.java
@@ -4,20 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class UniversityInsertConfig extends UniversityConfig {
 
     @Bean
-    public SimpleJdbcInsert insertTeacher() {
-        return new SimpleJdbcInsert(dataSource())
+    public SimpleJdbcInsert insertTeacher(DataSource dataSource) {
+        return new SimpleJdbcInsert(dataSource)
                 .withTableName("teachers")
                 .usingColumns("first_name", "last_name")
                 .usingGeneratedKeyColumns("id");
     }
 
     @Bean
-    public SimpleJdbcInsert insertGroup() {
-        return new SimpleJdbcInsert(dataSource())
+    public SimpleJdbcInsert insertGroup(DataSource dataSource) {
+        return new SimpleJdbcInsert(dataSource)
                 .withTableName("groups")
                 .usingColumns("name")
                 .usingGeneratedKeyColumns("id");
