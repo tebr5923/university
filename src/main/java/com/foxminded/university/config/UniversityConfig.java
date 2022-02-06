7b9c2379fbf24b204afe5825d8539f40bffc0f00
Dmitry Tebryaev <tebr5923@mail.ru>
2022-02-06 20:14:12 +0700

    add UniversityDaoConfig

--- src/main/java/com/foxminded/university/config/UniversityConfig.java
+++ src/main/java/com/foxminded/university/config/UniversityConfig.java
@@ -1,12 +1,15 @@
 package com.foxminded.university.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(UniversityDataSourceConfig.class)
-@ComponentScan("com.foxminded.university")
+@Import({
+        UniversityDataSourceConfig.class,
+        UniversityDaoConfig.class,
+        UniversityMapperConfig.class,
+        UniversityInsertConfig.class
+})
 public class UniversityConfig {
 
 }
