88f67200ff3194b91f53acaf312dbd2adb7c394a
Dmitry Tebryaev <tebr5923@mail.ru>
2022-02-06 20:16:36 +0700

    del @ComponentScan from testConfig aan add @Import

--- src/test/java/com/foxminded/university/config/UniversityTestConfig.java
+++ src/test/java/com/foxminded/university/config/UniversityTestConfig.java
@@ -1,11 +1,11 @@
 package com.foxminded.university.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 
 @Configuration
-@ComponentScan("com.foxminded.university")
+@Import(UniversityConfig.class)
 @TestPropertySource("classpath:db.properties")
 public class UniversityTestConfig {
 
