4c235d84cb3498d002f0c1ca8576d3ca47731543
Dmitry Tebryaev <tebr5923@mail.ru>
2022-02-13 22:37:39 +0700

    add getById into ClassroomService

--- src/main/java/com/foxminded/university/service/ClassroomService.java
+++ src/main/java/com/foxminded/university/service/ClassroomService.java
@@ -1,5 +1,10 @@
 package com.foxminded.university.service;
 
+import com.foxminded.university.domain.model.Classroom;
+
+import java.util.Optional;
+
 public interface ClassroomService {
+    Optional<Classroom> getById(Integer id);
 
 }
